302f3fc988c8b4250f8175f5d60ae7d7602759c5
chiennt1024 <chiennt1024@gmail.com>
2021-08-27 17:00:11 +0700

    session storage home fetch, movie fetch

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -1,6 +1,8 @@
 import { useState, useEffect, useRef } from "react";
 // API
 import API from '../API';
+// Helpers
+import { isPersistedState } from "../helpers";
 
 const initialState = {
     page: 0,
@@ -39,6 +41,16 @@ export const useHomeFetch = () => {
 
     // Initial and search
     useEffect(() => {
+        if(!searchTerm) {
+            const sessionState = isPersistedState('homeState');
+
+            if(sessionState) {
+                console.log('Grabbing from sessionStorage')
+                setState(sessionState);
+                return;
+            }
+        }
+        console.log('Grabbing from API')
         setState(initialState);
         fetchMovies(1, searchTerm);
     }, [searchTerm])
@@ -50,6 +62,11 @@ export const useHomeFetch = () => {
         fetchMovies(state.page + 1, searchTerm);
         setIsLoadingMore(false);
     }, [isLoadingMore])
+
+    // White to sessionStorage
+    useEffect(() => {
+        if(!searchTerm) sessionStorage.setItem('homeState', JSON.stringify(state));
+    }, [searchTerm, state])
 
     return { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore}
 }
